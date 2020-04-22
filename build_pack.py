0baffc78ce9ab4f932365243e415c7fce8ac37a8
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2020-04-22 17:25:17 +0200

    bump up date and version number

--- build_pack.py
+++ build_pack.py
@@ -14,8 +14,8 @@ from collections import Counter
 
 
 __author__ = "aquaman"
-__date__ = "2019/09/30"
-__version__ = "$Revision: 3.3"
+__date__ = "2020/04/22"
+__version__ = "$Revision: 3.5"
 
 
 # *********************************************************************#
